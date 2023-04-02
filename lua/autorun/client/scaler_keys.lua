5db1565d5833b6d9e2cb2cf0b0937c381daf7110
Cpt-Hazama <Caleb123459@gmail.com>
2023-04-02 15:04:59 -0400

    Global Fix

--- lua/autorun/client/scaler_keys.lua
+++ lua/autorun/client/scaler_keys.lua
@@ -35,12 +35,42 @@
 ------------------==============================================================================================================================------------------
 
 CreateClientConVar("scaler_enabled", "1", true, false)
+CreateClientConVar("scaler_global", "1", true, true)
 
 Scaler = Scaler or {}
 local var = GetConVar("scaler_global")
 Scaler.Global = var && var:GetBool() or false
 
-local dev = false
+local enabled = GetConVar("scaler_enabled")
+if enabled && enabled:GetBool() then
+    Scaler.Global = true
+    hook.Add("PostDrawOpaqueRenderables","ScalerKeys_Global",function()
+        for _,v in pairs(ents.GetAll()) do
+            if v.ScalerKeysTable != nil then
+                Scaler.ApplyScalerKeys(v)
+            end
+        end
+    end)
+    print("ScalerKeys: Enabled")
+else
+    Scaler.Global = false
+    hook.Remove("PostDrawOpaqueRenderables","ScalerKeys_Global")
+    for _,ent in pairs(ents.GetAll()) do
+        if ent.ScalerKeysTable != nil then
+            for k,v in pairs(ent.ScalerKeysTable) do
+                for k2,v2 in pairs(v) do
+                    bone = ent:LookupBone(k2)
+                    if bone != nil && ent:GetBoneName(bone) != "__INVALIDBONE__" then
+                        ent:ManipulateBoneScale(bone,defScale)
+                    end
+                end
+            end
+        end
+    end
+    print("ScalerKeys: Disabled")
+end
+
+local dev = true
 
 local defScale = Vector(1,1,1)
 
@@ -79,6 +109,27 @@ Scaler.GetAnimationData = function(ent,seq)
 	return nil
 end
 
+/*
+    // Example Tables
+
+    ENT.ScalerKeysTable = { -- This table contains all original scaler data
+        ["type1"] = {
+            ["ago"] = {
+                defScale,
+                Vector(1.36, 1, 1),
+                defScale,
+            }
+        }
+    }
+
+    ENT.ScalerKeysAdjustmentTable = { -- This table is used to apply specific scaler data to other animations
+        ["type1_a"] = { -- New animation
+            Parent = "type1", -- Animation we are taking from
+            StartFrame = 1, -- Frame we are starting from, the EndFrame will be calculated based on the length of the new animation
+        }
+    }
+*/
+
 Scaler.ApplyScalerKeys = function(ent) -- Place in client-side CustomOnDraw function
     if !IsValid(ent) then
         return
@@ -110,40 +161,68 @@ Scaler.ApplyScalerKeys = function(ent) -- Place in client-side CustomOnDraw func
     -- Create a list to store the bones that are being used in the active sequence
     local activeBones = {}
 
-    for k,v in pairs(ent.ScalerKeysTable) do
-        if k == seqName then
-            for k2,v2 in pairs(v) do
-                bone = ent:LookupBone(k2)
-                if bone != nil && ent:GetBoneName(bone) != "__INVALIDBONE__" then
-                    scale = v2[frame -1]
-                    if scale != nil then
-                        if dev then
-                            print(ent,bone,"[" .. seqName .. " {Frame - " .. frame .. "/" .. frameCount .. "}] Applying scale to bone " .. ent:GetBoneName(bone) .. " of " .. tostring(scale))
+    -- Check if ENT.ScalerKeysAdjustmentTable exists
+    -- if ent.ScalerKeysAdjustmentTable != nil && ent.ScalerKeysAdjustmentTable[seqName] != nil then
+    --     local adjustmentData = ent.ScalerKeysAdjustmentTable[seqName]
+    --     -- Get the parent animation data
+    --     local parentData = ent.ScalerKeysTable[adjustmentData.Parent]
+    --     if parentData != nil then
+    --         // Because the new animation is a different length, we need to calculate the current frame based on the new animation
+    --         local adjustedFrame = frame + adjustmentData.StartFrame -1
+    --         local endFrame = adjustmentData.StartFrame + frameCount - 1
+    --         for k2,v2 in pairs(parentData) do
+    --             bone = ent:LookupBone(k2)
+    --             if bone != nil && ent:GetBoneName(bone) != "__INVALIDBONE__" then
+    --                 -- Use the parent animation data within the start and end frames
+    --                 scale = v2[adjustmentData.StartFrame -1 + adjustedFrame]
+    --                 -- print(scale != nil,adjustedFrame,adjustmentData.StartFrame,endFrame)
+    --                 if scale != nil && adjustedFrame >= adjustmentData.StartFrame && adjustedFrame <= endFrame then
+    --                     if dev then
+    --                         print(ent,bone,"[" .. seqName .. " {Frame - " .. adjustedFrame .. "/" .. frameCount +adjustmentData.StartFrame .. "}] Applying scale to bone " .. ent:GetBoneName(bone) .. " of " .. tostring(scale))
+    --                     end
+    --                     ent:ManipulateBoneScale(bone,scale)
+    --                     -- Add the bone to the list of active bones
+    --                     table_insert(activeBones, bone)
+    --                 end
+    --             end
+    --         end
+    --     end
+    -- else
+        for k,v in pairs(ent.ScalerKeysTable) do
+            if k == seqName then
+                for k2,v2 in pairs(v) do
+                    bone = ent:LookupBone(k2)
+                    if bone != nil && ent:GetBoneName(bone) != "__INVALIDBONE__" then
+                        scale = v2[frame -1]
+                        if scale != nil then
+                            if dev then
+                                -- print(ent,bone,"[" .. seqName .. " {Frame - " .. frame .. "/" .. frameCount .. "}] Applying scale to bone " .. ent:GetBoneName(bone) .. " of " .. tostring(scale))
+                            end
+                            ent:ManipulateBoneScale(bone,scale)
+                            -- Add the bone to the list of active bones
+                            table_insert(activeBones, bone)
                         end
-                        ent:ManipulateBoneScale(bone,scale)
-                        -- Add the bone to the list of active bones
-                        table_insert(activeBones, bone)
                     end
                 end
-            end
-        else
-            if ent.ScalerKeys_LastLoopSequence == seqName /*or ent.ScalerKeys_LastSequence == seqName*/ then
-                continue
-            end
-            for k2,v2 in pairs(v) do
-                bone = ent:LookupBone(k2)
-                if bone != nil && ent:GetBoneName(bone) != "__INVALIDBONE__" then
-                    -- Check if the bone is in the list of active bones before resetting
-                    if !table_HasValue(activeBones, bone) then
-                        if dev then
-                            print("[" .. seqName .. " {Frame - " .. frame .. "/" .. frameCount .. "}] Resetting scale to bone " .. ent:GetBoneName(bone) .. " of " .. tostring(defScale))
+            else
+                if ent.ScalerKeys_LastLoopSequence == seqName /*or ent.ScalerKeys_LastSequence == seqName*/ then
+                    continue
+                end
+                for k2,v2 in pairs(v) do
+                    bone = ent:LookupBone(k2)
+                    if bone != nil && ent:GetBoneName(bone) != "__INVALIDBONE__" then
+                        -- Check if the bone is in the list of active bones before resetting
+                        if !table_HasValue(activeBones, bone) then
+                            if dev then
+                                -- print("[" .. seqName .. " {Frame - " .. frame .. "/" .. frameCount .. "}] Resetting scale to bone " .. ent:GetBoneName(bone) .. " of " .. tostring(defScale))
+                            end
+                            ent:ManipulateBoneScale(bone,defScale)
                         end
-                        ent:ManipulateBoneScale(bone,defScale)
                     end
                 end
             end
         end
-    end
+    -- end
 end
 
 cvars.AddChangeCallback("scaler_enabled", function(convar_name, value_old, value_new)
